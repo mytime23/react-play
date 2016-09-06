e1d5263f032db469caa0206f0a9da62a088c5b0e
Tim Wan <ord365@gmail.com>
2016-09-06 22:43:31 +0800

    Change build file rename budle and add React test component

--- app/index.js
+++ app/index.js
@@ -1,1 +1,41 @@
-document.getElementById('content').innerHTML = '<h1>Hello React Play</h1>';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var TitleComponent = React.createClass({
+    render : function() {
+        return (
+            <h1>React Play Application</h1>
+        );
+    }
+})
+
+var NavComponent = React.createClass({
+    render : function() {
+        return (
+            <div className="nav">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Docs</li>
+                    <li>Support</li>
+                </ul>
+            </div>
+        );
+    }
+})
+
+var AppComponent = React.createClass({
+    render : function() {
+        return (
+            <div className="AppComponent">
+                <TitleComponent />
+                <NavComponent />
+            </div>    
+        );
+    }
+});
+
+ReactDOM.render(
+    <AppComponent />,
+    document.getElementById('content')
+);
